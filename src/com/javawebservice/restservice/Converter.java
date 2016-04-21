67e670837eca971466cfc9080a295373d926c1fa
SrikanthJasti <jastisrikanth36@gmail.com>
2016-04-20 17:16:59 -0700

    First Commit

---
+++ src/com/javawebservice/restservice/Converter.java
@@ -0,0 +1,41 @@
+package com.javawebservice.restservice;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("ConversionService")
+public class Converter {
+	@GET
+	@Path("/InchToFeet/{i}")
+	@Produces(MediaType.TEXT_XML)
+	public String inchToFeet(@PathParam("i")int i){
+		
+		int inch = i;
+		double feet = 0;
+		feet = (double)inch/12;
+		return "<InchToFeetService>"
+				+"<Inch>"+inch+"</Inch>"
+				+"<Feet>"+feet+"</Feet>"
+				+"</InchToFeetService>";
+		
+	}
+	
+	@GET
+	@Path("/FeetToInch/{f}")
+	@Produces(MediaType.TEXT_XML)
+	public String feetToInch(@PathParam("f") int f){
+		
+		int inch = 0;
+		int feet = f;
+		inch = feet*12;
+		
+		return "<FeetToInchService>"
+				+"<Feet>"+feet+"</Feet>"	
+				+"<Inch>"+inch+"</Inch>"
+				+"</FeetToInchService>";
+	}
+	
+}
